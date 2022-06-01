b7b8bd98e5c2b812ed4b00564facb9f3f7368044
AlviseFerro <o0magefa0o@gmail.com>
2022-06-01 18:53:28 +0200

    connessione db macchina virtuale

--- index/php_index_accesso.php
+++ index/php_index_accesso.php
@@ -5,7 +5,11 @@
 
     try
     {
-        $connection = new PDO("mysql:host=localhost;dbname=PROGETTO_PERSONALE","root","");
+        //sul mac
+        //$connection = new PDO("mysql:host=localhost;dbname=PROGETTO_PERSONALE","root","");
+        
+        // sulla macchina virtuale del sito
+        $connection = new PDO("mysql:host=localhost;dbname=progetto_personale","root","");
         $query = "SELECT * FROM Promemoria, Utente WHERE Utente.email = $email AND Utente.password = $password";
         $result = $connection->query($query);
         if( $result->fetchColumn() > 0)
